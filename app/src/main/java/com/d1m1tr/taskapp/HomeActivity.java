3c721c64ebdc97b8803accaf89b6fa59368df1b0
Dimitar Tsvetkov <tsvetkovdimitar@gmai.com>
2019-02-20 22:00:02 +0000

    Toolbar added

--- app/src/main/java/com/d1m1tr/taskapp/HomeActivity.java
+++ app/src/main/java/com/d1m1tr/taskapp/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.d1m1tr.taskapp;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Task App");
     }
+    
 }
