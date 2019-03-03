6396cbfbdaf22dc2f6125fb89da9d11dda941001
Dimitar Tsvetkov <tsvetkovdimitar@gmai.com>
2019-03-03 20:02:03 +0000

    Floating button added

--- app/src/main/java/com/d1m1tr/taskapp/HomeActivity.java
+++ app/src/main/java/com/d1m1tr/taskapp/HomeActivity.java
@@ -1,12 +1,16 @@
 package com.d1m1tr.taskapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    private FloatingActionButton penBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,16 @@ public class HomeActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar_home);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Task App");
+
+        penBtn = findViewById(R.id.pen_btn);
+
+        penBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
-    
+
 }
