6cec5fb0c5d53ee2a219df8ec953f038b870b36d
Dimitar Tsvetkov <tsvetkovdimitar@gmai.com>
2019-03-04 10:37:04 +0000

    Task design error fixed

--- app/src/main/java/com/d1m1tr/taskapp/HomeActivity.java
+++ app/src/main/java/com/d1m1tr/taskapp/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.d1m1tr.taskapp;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
@@ -27,7 +29,17 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                AlertDialog.Builder myDialog = new AlertDialog.Builder(HomeActivity.this);
 
+                LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
+
+                View myView = inflater.inflate(R.layout.inputfireld, null);
+
+                myDialog.setView(myView);
+
+                AlertDialog dialog = myDialog.create();
+
+                myDialog.show();
 
             }
         });
