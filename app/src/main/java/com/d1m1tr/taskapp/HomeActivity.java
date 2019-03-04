fe4acae05ff8d3db9cf66ea6471e73c83c43dcaf
Dimitar Tsvetkov <tsvetkovdimitar@gmai.com>
2019-03-04 11:48:36 +0000

    Input fields connected with floating action button

--- app/src/main/java/com/d1m1tr/taskapp/HomeActivity.java
+++ app/src/main/java/com/d1m1tr/taskapp/HomeActivity.java
@@ -1,12 +1,15 @@
 package com.d1m1tr.taskapp;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -39,7 +42,35 @@ public class HomeActivity extends AppCompatActivity {
 
                 AlertDialog dialog = myDialog.create();
 
-                myDialog.show();
+                final EditText title = myView.findViewById(R.id.title);
+                final EditText note = myView.findViewById(R.id.note);
+
+                Button saveButton = myView.findViewById(R.id.btn_save);
+
+                saveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        String mTitle = title.getText().toString().trim();
+                        String mNote = note.getText().toString().trim();
+
+                        if(TextUtils.isEmpty(mTitle)){
+
+                            title.setError("Required Field..");
+                            return;
+
+                        }
+                        if(TextUtils.isEmpty(mNote)){
+
+                            note.setError("Required Field..");
+                            return;
+
+                        }
+
+                    }
+                });
+
+                dialog.show();
 
             }
         });
